95811a8791a60475a0115e0860f86bbc4d322c83
verythinice <quake.grizzly@gmail.com>
2013-08-05 16:48:21 -0700

    Changed passing parameter from path to get.

--- src/main/java/com/cloudexplorer/controller/ListFiles.java
+++ src/main/java/com/cloudexplorer/controller/ListFiles.java
@@ -2,22 +2,21 @@ package com.cloudexplorer.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import com.cloudexplorer.model.AWSService;
 import com.cloudexplorer.model.CloudService;
+import com.cloudexplorer.model.StorageTypeChecker;
 
 @Path("/object")
 @Produces(MediaType.APPLICATION_JSON)
 public class ListFiles {
 	@GET
-	@Path("/{storageService}/{storageName}")
-	public String listObjects(@PathParam("storageService") String storageService, @PathParam("storageName") String storageName) {
+	@Path("/list")
+	public String listObjects(@QueryParam("type") String storageService, @QueryParam("name") String storageName) {
 		CloudService service;
-		//TODO Service check call
-		service = new AWSService();
+		service = StorageTypeChecker.returnCorrectStorageType(storageService);
 		String output = service.listFiles(storageName);
 		return output;
 	}//this is a comment
--- src/main/java/com/cloudexplorer/controller/ListStorage.java
+++ src/main/java/com/cloudexplorer/controller/ListStorage.java
@@ -2,29 +2,22 @@ package com.cloudexplorer.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
 
-import com.cloudexplorer.model.AWSService;
 import com.cloudexplorer.model.CloudService;
+import com.cloudexplorer.model.StorageTypeChecker;
 
 @Path("/storage")
 public class ListStorage{
 	
 	@GET
-	@Path("/{param}")
+	@Path("/list")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String listBuckets(@PathParam("param") String msg) {
+	public String listBuckets(@QueryParam("type") String storageService) {
 		CloudService service;
-		//TODO service check call
-		if (msg.equals("aws")){
-			service = new AWSService();
-		}
-		else{
-			return ("Invalid storage type");
-		}
+		service = StorageTypeChecker.returnCorrectStorageType(storageService);
 		String output = service.listStorage();
 		return output;
 	}
