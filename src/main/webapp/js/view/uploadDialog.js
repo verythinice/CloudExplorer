b2c1fe7a78ee948343ca68be8828f7da77beedd6
BS-Repo <slamym@ymail.com>
2013-08-27 17:41:59 -0700

    More upload dialog improvement

--- src/main/webapp/js/view/uploadDialog.js
+++ src/main/webapp/js/view/uploadDialog.js
@@ -7,6 +7,7 @@ define(['jquery', 'underscore', 'backbone', 'pubSubEvents', 'view/modalDialog', 
 				_.bindAll(this, 'render');
 				this.template = _.template(uploadDialogTemplate);
 				this.fileList = [];
+				this.fileNum = 0;
 
 				// TODO No validation yet
 				// Backbone.Validation.bind( this, {valid:this.hideError,
@@ -27,9 +28,14 @@ define(['jquery', 'underscore', 'backbone', 'pubSubEvents', 'view/modalDialog', 
 			selectFile: function(event) {
 				var fileList = document.getElementById('fileList');
 				for (var i = 0; i < fileList.files.length; i ++) {
-					$('#selectedFiles').append('<tr><td>' + fileList.files[i].name + '</td><td>' + fileList.files[i].size + '</td><td></td></tr><tr><td colspan="3"></td></tr>');
+					$('#selectedFiles').append('<tr><td id="file_' + this.fileNum + '">' + fileList.files[i].name + '</td><td>' + fileList.files[i].size + 
+							'</td></tr><tr><td id="progress_' + this.fileNum + 
+							'"><div id="bar_' + this.fileNum + 
+							'" class="bar"></div></td><td id="percent_' + this.fileNum + '"></td></tr>');
 					this.fileList.push(fileList.files[i]);
+					this.fileNum++;
 				}
+				$('#numFileSelected').html(this.fileNum);
 			},
 			
 			uploadFile: function(event) {
@@ -39,31 +45,37 @@ define(['jquery', 'underscore', 'backbone', 'pubSubEvents', 'view/modalDialog', 
 	            // TODO Close dialog, show progress bar.
 				
 		        var uploadProgress = function(event) {
-		        	// TODO Need progress bar.
+		        	var name = event.target.myParam;
+		        	
 		            if (event.lengthComputable) {
 		                var percentComplete = Math.round(event.loaded * 100 / event.total);
-		                //document.getElementById('progressNumber').innerHTML = percentComplete.toString() + '%';
-		                //document.getElementById('prog').value = percentComplete;
-		            	console.log('uploadProgress: ' + percentComplete.toString() + '%');
+		                $('#bar_' + name).width(percentComplete + '%');
+		                $('#percent_' + name).html(percentComplete.toString() + '%');
+		                
+		            	console.log('uploadProgress: ' + percentComplete.toString() + '%' + ', ' + name);
 		            }
 		            else {
-		                //document.getElementById('progressNumber').innerHTML = 'unable to compute';
 		            	console.log('uploadProgress unable to compute');
 		            }
 		        }
 
 		        var uploadComplete = function(event) {
-		        	// TODO Need to track all files uploaded.
-					that.hideModal();
+		        	var name = event.target.myParam;
+
+	                $('#bar_' + name).width('100%');
+	                $('#percent_' + name).html('100%');
+
+	                // TODO Need to track all files uploaded.
+					//that.hideModal();
 					// TODO Need to wait a little before refreshing screen.
 					/*
 					for (var i = 1; i < 10000; i++) {
 						var j = i;
 					}
 					*/
-		        	PubSubEvents.trigger('refreshRightPane');
+		        	//PubSubEvents.trigger('refreshRightPane');
 		            /* This event is raised when the server send back a response */
-		            console.log('uploadComplete: ' + event.target.responseText);
+		            console.log('uploadComplete: ' + name);
 		        }
 
 		        var uploadFailed = function(event) {
@@ -75,14 +87,19 @@ define(['jquery', 'underscore', 'backbone', 'pubSubEvents', 'view/modalDialog', 
 		        	// TODO Cancel handling.
 		            alert("The upload has been canceled by the user or the browser dropped the connection.");
 		        }
+		        
+		        $('#browseFiles').hide();
+		        $('#uploadButton').hide();
+		        $('.bar').show();
 				
 				for (var i = 0; i < this.fileList.length; i ++) {
-					var formData = new FormData();
+		            var formData = new FormData();
 					formData.append('type', 'aws');
 					formData.append('storageName', 'bstestbucket');
 					formData.append('file', this.fileList[i]);
 				    
 		            var xhr = new XMLHttpRequest();
+		            xhr.upload.myParam = i;
 		            xhr.upload.addEventListener("progress", uploadProgress, false);
 		            xhr.upload.addEventListener("load", uploadComplete, false);
 		            xhr.addEventListener("error", uploadFailed, false);
